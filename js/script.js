04f1aa752840b2e9c65ef13906a58898599820c9
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-11-08 16:50:33 +0100

    Generati indirizzi email e stampati in pagina

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            // email: '',
+            emails: [],
+        }
+    },
+    methods: {
+        callApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+                this.email = res.data.response;
+                this.emails.push(this.email);
+                console.log(this.emails);
+            })
+        }
+    },
+    created() {
+        for(let i = 0; i < 10; i++){
+            this.callApi();
+        }
+    }
+}).mount('#app')
